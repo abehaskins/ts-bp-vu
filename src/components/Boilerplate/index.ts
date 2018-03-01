bf51bf213aae0f248252a151f01d449c39c6f7f1
Abe Haskins <abeisgreat@abeisgreat.com>
2018-03-01 14:52:46 -0800

    Remove extra crap from boilerplate

--- src/components/Boilerplate/index.ts
+++ src/components/Boilerplate/index.ts
@@ -2,15 +2,9 @@
 import Vue from "vue";
 import { Component, Inject, Model, Prop, Watch } from "vue-property-decorator";
 
-// Typings for modules imported dynamically
-import FirebaseAppModule = require("firebase/app");
-
 @Component
 export default class Boilerplate extends Vue {
   name = "Boilerplate";
-  required = {
-    firebase: FirebaseAppModule
-  };
 
   async mounted() {}
 }
